58cc7ff7d9d09a9a5347f0e90aeecde7198f13d9
filipspl <stefaniak@gmail.com>
2018-01-20 08:06:23 +0100

    autocommit-sob, 20 sty 2018, 08:06:23 CET

--- noaa_conf.py
+++ noaa_conf.py
@@ -24,8 +24,8 @@ rtl_fm_path='/usr/bin/rtl_fm'
 
 # script tha will be used whle waiting for the next pass; set False if we just want to sleep
 # by default, this script will get the parameter of duration of the time to be run and the recent dongleShift
-#scriptToRunInFreeTime = False
-scriptToRunInFreeTime = "/home/filips/github/autowx2/bin/aprs.sh"
+scriptToRunInFreeTime = False
+#scriptToRunInFreeTime = "/home/filips/github/autowx2/bin/aprs.sh"
 
 # Dongle gain
 dongleGain='49.8'
