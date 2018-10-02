50b6728141b8a5e2a284e359f5fd8a30a6e498ae
SA7BNT <35367117+SA7BNT@users.noreply.github.com>
2018-10-02 20:36:23 +0200

    Update update-keps.sh
    Add tle https://www.amsat.org/amsat/ftp/keps/current/nasa.all

--- bin/update-keps.sh
+++ bin/update-keps.sh
@@ -9,11 +9,14 @@ source $scriptDir/basedir_conf.py
 
 TLEDIR=$baseDir/var/tle/
 
-rm $TLEDIR/weather.txt
-wget --no-check-certificate -r http://www.celestrak.com/NORAD/elements/weather.txt -O $TLEDIR/weather.txt
+#rm $TLEDIR/weather.txt
+#wget --no-check-certificate -r http://www.celestrak.com/NORAD/elements/weather.txt -O $TLEDIR/weather.txt
 
 #rm $TLEDIR/noaa.txt
 #wget -r http://www.celestrak.com/NORAD/elements/noaa.txt -O $TLEDIR/noaa.txt
+
+rm $TLEDIR/nasa.txt
+wget -r https://www.amsat.org/amsat/ftp/keps/current/nasa.all -O $TLEDIR/nasa.txt
 
 rm $TLEDIR/amateur.txt
 wget -r http://www.celestrak.com/NORAD/elements/amateur.txt -O $TLEDIR/amateur.txt
