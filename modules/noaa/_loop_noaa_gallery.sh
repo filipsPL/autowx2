e821b7700194f99e4e47565f3613a1a753b3dfcc
Filip Stefaniak <filips@achjoj.info>
2018-11-15 21:23:34 +0100

    image gallery improvements

---
+++ modules/noaa/_loop_noaa_gallery.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# NOAA sat pass gallery preparation
+# loop over passes in the given directory
+
+scriptDir="$(dirname "$(realpath "$0")")"
+source $scriptDir/basedir_conf.py
+source $baseDir/_listvars.sh
+
+# fileNameCore="$1"
+# satellite="$2"
+# start="$3"
+# duration="$4"
+# peak="$5"
+# azimuth="$6"
+# freq="$7"
+
+#imgdir=/home/filips/bin/autowx2/var/www/recordings/noaa/img/2018/11/14/
+#fileNameCore="20181114-1818_NOAA-18"
+#noaaDir="/home/filips/bin/autowx2/var/www/recordings/noaa/"
+
+imgdir="$1"
+noaaDir="/home/filips/bin/autowx2/var/www/recordings/noaa/"
+curdir=$(pwd)
+
+for logfile in $(ls $imgdir/*.log)
+do
+    fileNameCore=$(basename $logfile .log)
+    echo $fileNameCore
+    source $scriptDir/noaa_gallery.sh
+done
