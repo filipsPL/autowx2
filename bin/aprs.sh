a3c3a1490abe0b482cd1b379160ee7f52cde20f8
filips <stefaniak@gmail.com>
2019-05-04 15:44:08 +0200

    timestamping with ts

--- bin/aprs.sh
+++ bin/aprs.sh
@@ -16,4 +16,4 @@ dongleShift=$2
 mkdir -p $(dirname $logfile)
 
 
-timeout --kill-after=1 $duration rtl_fm $biast -f 144800000 -s 22050 -o 4 -p $dongleShift -g 49.6 | multimon-ng -a AFSK1200 -A -t raw - | tee -a $logfile
+timeout --kill-after=1 $duration rtl_fm $biast -f 144800000 -s 22050 -o 4 -p $dongleShift -g 49.6 | multimon-ng -a AFSK1200 -A -t raw - | ts '[%Y-%m-%d %H:%M:%S]' | tee -a $logfile 
