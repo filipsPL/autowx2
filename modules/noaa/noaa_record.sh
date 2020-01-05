f07bd463084da4430e6c53b6d364b16c0577fcf0
Thomas Bartleet <42772107+ThomasBartleet@users.noreply.github.com>
2020-01-05 12:47:16 +0000

    Created and implemented debugEcho function for shell scripts.
    Changes:
    - Created a function to easily toggle debug messages.
    - Implemented debugEcho function in noaa.sh and noaa_record.sh.

--- modules/noaa/noaa_record.sh
+++ modules/noaa/noaa_record.sh
@@ -8,20 +8,20 @@
 # recording here
 #
 
-echo "[DEBUG] Recording duration= ${duration}"
-echo "[DEBUG] biast= ${biast}"
-echo "[DEBUG] freq= ${freq}"
-echo "[DEBUG] sample= ${sample}"
-echo "[DEBUG] dongleGain= ${dongleGain}"
-echo "[DEBUG] dongleShift= ${dongleShift}"
+debugEcho "Recording duration= ${duration}"
+debugEcho "biast= ${biast}"
+debugEcho "freq= ${freq}"
+debugEcho "sample= ${sample}"
+debugEcho "dongleGain= ${dongleGain}"
+debugEcho "dongleShift= ${dongleShift}"
 
-echo "[DEBUG] Recording"
+debugEcho "Recording"
 timeout $duration rtl_fm $biast -f $freq -s $sample -g $dongleGain -F 9 -A fast -E offset -p $dongleShift $recdir/$fileNameCore.raw | tee -a $logFile
 
 #
 # transcoding here
 #
-echo "[DEBUG] Transcoding"
+debugEcho "Transcoding"
 sox -t raw -r $sample -es -b 16 -c 1 -V1 $recdir/$fileNameCore.raw $recdir/$fileNameCore.wav rate $wavrate | tee -a $logFile
 touch -r $recdir/$fileNameCore.raw $recdir/$fileNameCore.wav
 rm $recdir/$fileNameCore.raw
